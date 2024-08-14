d3b7c494bed9ca2d20820219803c190064d38fa7
Kenward Terhemba <codewithkenward@gmail.com>
2024-08-14 13:44:37 +0100

    Implementing User Authentication in Django update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -25,12 +25,12 @@ class LibraryDetailView(DetailView):
 class register(View):
     def get(self, request):
         form = UserCreationForm()
-        return render(request, 'register.html', {'form': form})
+        return render(request, 'relationship_app/register.html', {'form': form})
 
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login')  # Redirect to login after registration
-        return render(request, 'register.html', {'form': form})
+            return redirect('login')  
+        return render(request, 'relationship_app/register.html', {'form': form})
