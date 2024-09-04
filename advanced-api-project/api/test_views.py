550b0c4ec6aacf370d94995a77a97aba06c44e83
Kenward Terhemba <codewithkenward@gmail.com>
2024-09-04 21:21:50 +0100

    API development:Writing Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,16 +1,14 @@
-from django.test import TestCase
 from django.urls import reverse
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from api.models import Book, Author
-from rest_framework.test import APIClient
 
-class BookTestCase(TestCase):
+class BookTestCase(APITestCase):
 
     def setUp(self):
         # Create a user and log them in
-        self.client = APIClient()
         self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.client.login(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
 
         # Create an author
         self.author = Author.objects.create(name='Chinua Achebe')
@@ -45,13 +43,11 @@ class BookTestCase(TestCase):
         self.assertEqual(response.status_code, 204)  # HTTP 204 No Content
         self.assertEqual(Book.objects.count(), 0)  # Check if the book was deleted
 
-    
     def test_filter_books(self):
         url = reverse('book-list')
         response = self.client.get(url, {'title': 'Things Fall Apart'})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 1)  # Only one book should match the filter
-
 
     def test_search_books(self):
         url = reverse('book-list')
@@ -65,8 +61,6 @@ class BookTestCase(TestCase):
         response = self.client.get(url, {'author__name__icontains': 'Achebe'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Things Fall Apart')
-    
-
 
     def test_order_books(self):
         # Add another book to test ordering
@@ -80,17 +74,17 @@ class BookTestCase(TestCase):
         # Ensure publication_year is not None before comparison
         valid_books = [book for book in books if book['publication_year'] is not None]
         self.assertTrue(all(valid_books[i]['publication_year'] <= valid_books[i+1]['publication_year']
-                            for i in range(len(valid_books)-1)))
+                           for i in range(len(valid_books)-1)))
 
     def test_permissions(self):
         # Testing without login (should be forbidden)
-        self.client.logout()
+        self.client.force_authenticate(user=None)
         url = reverse('book-create')
         data = {'title': 'New Book', 'publication_year': 2024, 'author': self.author.id}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 403)  # HTTP 403 Forbidden for unauthenticated user
 
         # Testing with login (should be successful)
-        self.client.login(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 201)  # HTTP 201 Created
