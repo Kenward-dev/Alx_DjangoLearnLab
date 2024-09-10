ae87db75c23e0da9fdec59602ded0a5c732d4aca
Kenward Terhemba <codewithkenward@gmail.com>
2024-09-10 17:28:32 +0100

    Building a Complete Django Application:Implementing Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
