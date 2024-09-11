c9dcaab18eee6847c13e19513ba937c453a3ee2d
Kenward Terhemba <codewithkenward@gmail.com>
2024-09-11 17:11:59 +0100

    Building a Complete Django Application:Adding 'Comment' Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -22,7 +22,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
