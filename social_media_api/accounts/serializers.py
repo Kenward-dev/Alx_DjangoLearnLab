65f6959220e39daac8ce76a97ff99efb1c722186
Kenward Terhemba <codewithkenward@gmail.com>
2024-09-16 13:33:20 +0100

    Building and deploying a Django API:New project setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token')
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
